10c1eab2ef7b1f6f7c16750a9d8db84b1f26d61d
Konstantin K <29kostja.k@gmail.com>
2025-01-09 23:20:09 +0300

    добавил консольку для вывода результатов

---
+++ Shape-Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shape_Library;
+using Shape_Library.Shapes;
+
+
+var shapeHandlers = new List<IShapeInfo>()
+{
+    new CircleHandler(new Circle(1)),
+    new CircleHandler(new Circle(2)),
+    new CircleHandler(new Circle(0)),
+    new TriangleHandler(new Triangle(5,3,4)),
+    new TriangleHandler(new Triangle(6,6,6)),
+    new TriangleHandler(new Triangle(5,5,8)),
+    new TriangleHandler(new Triangle(1,2,3)),
+};
+
+foreach (var handler in shapeHandlers)
+{
+    Console.WriteLine(handler.GetInfo());
+}
+
